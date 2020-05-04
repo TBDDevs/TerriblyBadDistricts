7a38e097e5d6ffe5d898cf962afc837c1b72d1e7
Noah Rutan <noahrutan2@gmail.com>
2020-05-04 02:39:47 +0000

    I have added more nuance to the model for classifying instances.

--- dataProcessor.py
+++ dataProcessor.py
@@ -1,14 +1,29 @@
 import geopandas
 
-#load data from maryland census site
-url = "https://opendata.arcgis.com/datasets/bbe7d09a81fc40c8a7c9f4c80155842e_0.geojson"
-df = geopandas.read_file(url)
+def classifier(row):
+        # if percentage of black population is greater than 14
+        if row['PNHB'] > 14:
+            # if percentage of senior population is over 31
+            if row['PCTPOP65_'] > 31:
+                return "rep"
+            else:
+                return "dem"
+        else:
+            # if percentages of hispanic and asian populations is greater than 16
+            if row['PNHA'] + row['PHISP'] > 16:
+                return 'dem'
+            else:
+                return "rep"
 
-#this is just a temporary way to classify instances 
-#assign row value based on percentage of non hispanic black persons
-df.loc[df['PNHB'] >= 20, 'party'] = 'dem' 
-df.loc[df['PNHB'] < 20, 'party'] = 'rep' 
+def getData():
+    # load data from maryland census site
+    url = "https://opendata.arcgis.com/datasets/bbe7d09a81fc40c8a7c9f4c80155842e_0.geojson"
+    df = geopandas.read_file(url)
 
-print(df.head())
+    # classifies instances by party
+    df["party"] = df.apply(classifier, axis=1)
 
-print(df['party'].value_counts())
+    # for checking number of each party
+    #print(df['party'].value_counts())
+
+    return df
